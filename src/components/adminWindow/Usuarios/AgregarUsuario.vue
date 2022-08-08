fb49e4a351022eda59fa4b71c078ede322ded392
Et3rnauta <guidocanevello@hotmail.com>
2022-08-07 21:57:57 -0300

    Crear Vista

--- src/components/adminWindow/Usuarios/AgregarUsuario.vue
+++ src/components/adminWindow/Usuarios/AgregarUsuario.vue
@@ -1,11 +1,75 @@
 <template>
-  <v-container grid-list-xs> Agregar Usuario </v-container>
+  <v-container grid-list-xs>
+    <v-row>
+      <v-col>
+        {{ msg }}
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col> <h2>Nuevo Usuario</h2> </v-col>
+
+      <v-col style="text-align: right">
+        <v-btn
+          color="success"
+          :loading="isLoading"
+          :disabled="isLoading"
+          @click="handleGuardarCambios"
+        >
+          Crear
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="nombre"
+          label="Nombre de Usuario"
+          prepend-icon="mdi-account-outline"
+          outlined
+          required
+          :disabled="isLoading"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="password"
+          label="Contraseña"
+          prepend-icon="mdi-lock"
+          outlined
+          required
+          :disabled="isLoading"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "AgregarUsuario",
 
-  data: () => ({}),
+  data: () => ({
+    nombre: "",
+    password: "",
+
+    isLoading: false,
+    msg: "",
+  }),
+
+  methods: {
+    async handleGuardarCambios() {
+      this.isLoading = true;
+
+      console.log("se agrega al user", this.nombre, this.password);
+
+      this.msg = "Exito";
+      this.isLoading = false;
+    },
+  },
 };
 </script>
