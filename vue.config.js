4b324bf13e23bb8e668c5e1dfffb556264750925
Et3rnauta <guidocanevello@hotmail.com>
2022-08-10 21:17:03 -0300

    Update from main pull

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = {
     'vuetify'
   ],
 
+  // TODO ver path para que funcione bien, si entro a un subpath se rompe todo
   publicPath: process.env.NODE_ENV === "production" ? "/prode2022_frontend/" : "/",
 }
