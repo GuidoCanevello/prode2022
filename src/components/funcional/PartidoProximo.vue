5868dacbe65428623879e7d91d70b33919ea7486
Et3rnauta <guidocanevello@hotmail.com>
2022-10-12 22:47:10 -0300

    Se reacomoda Partido Proximo para que sea Responsivo

--- src/components/funcional/PartidoProximo.vue
+++ src/components/funcional/PartidoProximo.vue
@@ -1,51 +1,71 @@
 <template>
-  <v-card>
+  <v-card v-if="DATA_PROX_PARTIDO">
     <v-card-title primary-title> Proximo Partido </v-card-title>
+
+    <v-card-subtitle>
+      Fecha: {{ fechaFormateada }} - Hora: {{ horaFormateada }}
+    </v-card-subtitle>
 
     <v-card-text>
       <template v-if="!DATA_PROX_PARTIDO"> Cargando </template>
 
       <template v-else>
         <v-row>
-          <v-col sm="3">
-            <v-row class="mt-0">
-              <v-col class="pt-0"> Fecha: {{ fechaFormateada }} </v-col>
-            </v-row>
-            <v-row class="mt-0">
-              <v-col class="pt-0"> Hora: {{ horaFormateada }} </v-col>
-            </v-row>
-          </v-col>
+          <v-spacer v-if="isScreenBeyondLarge" />
 
-          <v-col align-self="center">
+          <v-col lg="11">
             <v-row>
-              <v-col style="text-align: right" sm="1">
-                <bandera :code="DATA_PROX_PARTIDO.code1" />
+              <v-col>
+                <v-card outlined class="px-2 py-2">
+                  <v-row>
+                    <v-col style="text-align: right" cols="auto">
+                      <bandera :code="DATA_PROX_PARTIDO.code1" />
+                    </v-col>
+                    <v-col style="text-align: left">
+                      <!-- <h2>{{ DATA_PROX_PARTIDO.equipo1 }}</h2> -->
+                      <h2>Estados Unidos</h2>
+                    </v-col>
+                  </v-row>
+                </v-card>
               </v-col>
-              <v-col style="text-align: left">
-                <h2>{{ DATA_PROX_PARTIDO.equipo1 }}</h2>
-              </v-col>
-              <v-col style="text-align: center" sm="2">
+              <v-col
+                class="px-0"
+                style="text-align: center"
+                cols="auto"
+                align-self="center"
+              >
                 <h3>vs.</h3>
               </v-col>
-              <v-col style="text-align: right">
-                <h2>{{ DATA_PROX_PARTIDO.equipo2 }}</h2>
-              </v-col>
-              <v-col style="text-align: left" sm="1">
-                <bandera :code="DATA_PROX_PARTIDO.code2" />
+              <v-col>
+                <v-card outlined class="px-2 py-2">
+                  <v-row>
+                    <v-col style="text-align: right">
+                      <h2>{{ DATA_PROX_PARTIDO.equipo2 }}</h2>
+                    </v-col>
+                    <v-col style="text-align: left" cols="auto">
+                      <bandera :code="DATA_PROX_PARTIDO.code2" />
+                    </v-col>
+                  </v-row>
+                </v-card>
               </v-col>
             </v-row>
+          </v-col>
+          
+          <v-spacer v-if="isScreenBeyondLarge" />
+        </v-row>
+
+        <v-row>
+          <v-col class="pt-0" style="text-align: center" cols="12">
+            <h3>Tu Pronostico:</h3>
           </v-col>
         </v-row>
 
         <template v-if="DATA_PROX_PARTIDO.prediccion">
           <v-row>
-            <v-col class="pt-0" style="text-align: left" sm="3">
-              <h3>Tu Prediccion:</h3>
-            </v-col>
             <v-col class="pt-0" style="text-align: right">
               <h3>{{ DATA_PROX_PARTIDO.prediccion.golesEquipo1 }}</h3>
             </v-col>
-            <v-col class="pt-0" style="text-align: center" sm="2">
+            <v-col class="pt-0" style="text-align: center" cols="auto">
               <h3>-</h3>
             </v-col>
             <v-col class="pt-0" style="text-align: left">
@@ -56,7 +76,7 @@
 
         <v-row v-else>
           <v-col class="pt-0" style="text-align: center">
-            <h3>Sin Prediccion</h3>
+            <h3>Sin Pronostico</h3>
           </v-col>
         </v-row>
       </template>
@@ -79,23 +99,36 @@ export default {
     ...mapGetters(["DATA_PROX_PARTIDO"]),
 
     fechaFormateada() {
-      const fecha = this.DATA_PROX_PARTIDO.fecha;
-      const nombreDia = obtenerNombreDia(fecha.getDay()),
-        dia = addCero(fecha.getDate()),
-        mes = addCero(fecha.getMonth() + 1);
+      if (this.DATA_PROX_PARTIDO) {
+        const fecha = this.DATA_PROX_PARTIDO.fecha;
+        const nombreDia = obtenerNombreDia(fecha.getDay()),
+          dia = addCero(fecha.getDate()),
+          mes = addCero(fecha.getMonth() + 1);
 
-      return `${nombreDia} ${dia}/${mes}`;
+        return `${nombreDia} ${dia}/${mes}`;
+      } else {
+        return "";
+      }
     },
 
     horaFormateada() {
-      const fecha = this.DATA_PROX_PARTIDO.fecha;
-      const hora = addCero(fecha.getHours()),
-        minutos = addCero(fecha.getMinutes());
+      if (this.DATA_PROX_PARTIDO) {
+        const fecha = this.DATA_PROX_PARTIDO.fecha;
+        const hora = addCero(fecha.getHours()),
+          minutos = addCero(fecha.getMinutes());
 
-      return `${hora}:${minutos}`;
+        return `${hora}:${minutos}`;
+      } else {
+        return "";
+      }
+    },
+
+    isScreenBeyondLarge() {
+      return (
+        this.$vuetify.breakpoint.name === "lg" ||
+        this.$vuetify.breakpoint.name === "xl"
+      );
     },
   },
-
-  methods: {},
 };
 </script>
