387a9acb7f7a1beafd0a01f3bfb5528a039091c2
Et3rnauta <guidocanevello@hotmail.com>
2022-11-01 20:45:29 -0300

    Cambios de Nombre a pestaña Backend

--- src/components/adminWindow/MainWindow.vue
+++ src/components/adminWindow/MainWindow.vue
@@ -2,22 +2,22 @@
   <v-card outlined>
     <v-container grid-list-xs>
       <v-tabs>
-        <v-tab> ABM Partidos </v-tab>
-        <v-tab-item>
-          <abm-partidos />
-        </v-tab-item>
-
-        <v-tab> ABM Usuarios </v-tab>
-        <v-tab-item>
-          <abm-usuarios />
-        </v-tab-item>
-
-        <v-tab> ABM Grupos </v-tab>
+        <v-tab> Crear Grupos </v-tab>
         <v-tab-item>
           <abm-grupos />
         </v-tab-item>
 
-        <v-tab> ABM Equipos </v-tab>
+        <v-tab> Modificar Partidos </v-tab>
+        <v-tab-item>
+          <abm-partidos />
+        </v-tab-item>
+
+        <v-tab> Crear Usuarios </v-tab>
+        <v-tab-item>
+          <abm-usuarios />
+        </v-tab-item>
+
+        <v-tab> Modificar Equipos </v-tab>
         <v-tab-item>
           <abm-equipos />
         </v-tab-item>
