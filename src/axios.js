1a46d987f80aca7fc1c9d3eeac156ba515d9f8ed
Et3rnauta <guidocanevello@hotmail.com>
2022-11-03 20:31:23 -0300

    Preparar Deploy

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://192.168.0.229:3000/';
+axios.defaults.baseURL = 'https://prode2022-backend.herokuapp.com';
+// axios.defaults.baseURL = 'http://192.168.0.229:3000/';
 // axios.defaults.headers.common['Authorization'] = 'Bearer ' + 'ITSMORBINTIME';
