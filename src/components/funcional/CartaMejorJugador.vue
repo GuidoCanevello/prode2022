d5f9c122b024428772b62f671c2395d4fe708ee5
Et3rnauta <guidocanevello@hotmail.com>
2022-11-28 23:13:05 -0300

    Crear estructura de Vue

---
+++ src/components/funcional/CartaMejorJugador.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card>
+    <v-container fluid>
+      <v-row>
+        <v-col>
+          <h2>Mejor Jugador</h2>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-autocomplete
+            v-model="selectedPaisMJ"
+            :items="nombresPaisesMJ"
+            filled
+            label="Equipo"
+            @change="
+              () => {
+                console.log('cambioEquipo');
+              }
+            "
+          />
+        </v-col>
+        <v-col md="8">
+          <v-autocomplete
+            v-model="selectedJugadorMJ"
+            :items="nombresJugadoresMJ"
+            filled
+            label="Jugador"
+            @change="
+              () => {
+                console.log('cambioJugador');
+              }
+            "
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid>
+      <h2>Mejor Arquero</h2>
+    </v-container>
+
+    <v-container fluid>
+      <h2>Mejor Goleador</h2>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CartaMejorJugador",
+
+  data: () => ({
+    selectedPaisMJ: "",
+    selectedJugadorMJ: "",
+    nombresPaisesMJ: [],
+    nombresJugadoresMJ: [],
+  }),
+  
+  mounted() {
+    
+  },
+};
+</script>
---
+++ src/views/MVP.view.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-container fluid>
+            <h1>Voto de MVP</h1>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-0">
+      <v-col>
+        <carta-mejor-jugador />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import CartaMejorJugador from '../components/funcional/CartaMejorJugador.vue';
+
+export default {
+  name: "MVP",
+  components: { CartaMejorJugador },
+
+  data: () => ({}),
+};
+</script>
