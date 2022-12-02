88400a9f8340eedfc0cdff8f635e9f21c43c34f5
Et3rnauta <guidocanevello@hotmail.com>
2022-12-02 13:59:21 -0300

    Nuevo Back

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://prode2022-backend.herokuapp.com';
+axios.defaults.baseURL = 'https://prode2022-backend.onrender.com';
 // axios.defaults.baseURL = 'http://192.168.0.229:3000/';
 // axios.defaults.headers.common['Authorization'] = 'Bearer ' + 'ITSMORBINTIME';
