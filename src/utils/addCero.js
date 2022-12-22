a1ed27ea85e097128fd30206525ac50bd7cf66e9
Et3rnauta <guidocanevello@hotmail.com>
2022-12-22 12:42:16 -0300

    Nuevo Deploy

--- src/utils/addCero.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {Number} num El numero a agregar
- * 
- * @returns String con un cero agregado si el numero es de un solo digito
- */
-export default function (num) {
-    return `${num > 9 ? "" : "0"}${num}`
-}
